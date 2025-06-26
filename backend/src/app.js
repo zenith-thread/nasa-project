7b5b1b375d23cfc985ea5e4fd3234d86cbe52737
Zenith Hanif <zenithpkhanif@gmail.com>
2025-06-26 17:21:55 +0500

    Adding quick npm commands to automate the installing dependencies, starting both backend and frontend. Build and serving frontend on one port through express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,11 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import morgan from "morgan";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Routers
 import planetsRouter from "./routes/planetsRouter/planets.router.js";
@@ -12,11 +18,15 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use("/api", planetsRouter);
 
 app.get("/", (req, res) => {
-  res.json({ msg: "API working" });
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 export default app;
